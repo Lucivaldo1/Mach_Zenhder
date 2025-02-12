5afdf25a1abf7139fbf26130ae57bdf3df02fe4b
Lucivaldo <135773154+Lucivaldo1@users.noreply.github.com>
2025-02-11 21:33:55 -0300

    '1102'

--- derivative.py
+++ derivative.py
@@ -2,7 +2,7 @@ import numpy as np
 def derivative(data):
     derive = np.zeros(len(data[:, 1]))
     derive[1:-1] = (data[2:, 1] - data[:-2, 1]) / (data[2:, 0] - data[:-2, 0])  # central
-    derive[0] = (data[1, 1] - data[0, 1]) / (data[1, 0] - data[0, 0])  # foward for the first value
-    derive[-1] = (data[-1, 1] - data[-2, 1]) / (data[-1, 0] - data[-2, 0])  # backward for the last value
-
+    derive[0] = (data[1, 1] - data[0, 1]) / (data[1, 0] - data[0, 0])  # foward
+    derive[-1] = (data[-1, 1] - data[-2, 1]) / (data[-1, 0] - data[-2, 0])  # backward
     return np.transpose([data[:, 0], derive])
+
